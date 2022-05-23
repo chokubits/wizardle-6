a13a4b211f38a3b032a3c2fd14c6dc4916b9cce7
chokubits <miirutan1@gmail.com>
2022-05-23 16:16:54 +0700

    updated words

--- src/constants/wordlist.ts
+++ src/constants/wordlist.ts
@@ -1,7 +1,15 @@
 export const WORDS = [
-  'mitile', 'rutile', 'voxnox', 'arthim', 'memini', 'wizard', 'mister', 'turner', 'lennox', 'manual', 'spells', 'figaro', 'knight', 'riquet', 'sweets', 'mirror', 'knives', 'garcia', 'memory', 'meiuva', 'forset', 'dragon', 'viesse', 'rambul', 'repsev', 'aivulp', 'matzah', 'family', 'doctor', 'morito', 'bubbles', 'clouds', 'scythe', 'pocket', 'kiseru', 'sewing', 'bottle', 'stella', 'palace', 'regalo', 'silver', 'clinic', 'prayer', 'swords', 'ballad', 'sanrio', 'collab', 'mithra', 'church', 'castle', 'bandit', 'arthur'
+  'mitile', 'rutile', 'voxnox', 'arthim', 'memini', 'wizard', 'mister', 'turner', 'lennox', 'manual', 'spells', 'figaro', 'knight', 'riquet', 'sweets', 'mirror', 'knives', 'garcia', 'memory', 'meiuva', 'forset', 'dragon', 'viesse', 'rambul', 'repsev', 'aivulp', 'matzah', 'family', 'doctor', 'morito', 'bubbles', 'clouds', 'scythe', 'pocket', 'kiseru', 'sewing', 'bottle', 'stella', 'palace', 'regalo', 'silver', 'clinic', 'prayer', 'swords', 'ballad', 'sanrio', 'collab', 'mithra', 'church', 'castle', 'bandit', 'arthur', 'amores',
+  'pajama',
+  'cavern',
+  'master',
+  'winter',
 ]
 
 export const EXTREME_WORDS = [
-  'mitile', 'rutile', 'voxnox', 'arthim', 'memini', 'wizard', 'mister', 'turner', 'lennox', 'manual', 'spells', 'figaro', 'knight', 'riquet', 'sweets', 'mirror', 'knives', 'garcia', 'memory', 'amores', 'meiuva', 'forset', 'dragon', 'viesse', 'rambul', 'repsev', 'aivulp', 'matzah', 'family', 'doctor', 'morito', 'bubbles', 'clouds', 'scythe', 'pocket', 'kiseru', 'sewing', 'bottle', 'stella', 'palace', 'regalo', 'silver', 'clinic', 'prayer', 'swords', 'ballad', 'sanrio', 'collab', 'mithra', 'church', 'castle', 'bandit', 'arthur'
+  'mitile', 'rutile', 'voxnox', 'arthim', 'memini', 'wizard', 'mister', 'turner', 'lennox', 'manual', 'spells', 'figaro', 'knight', 'riquet', 'sweets', 'mirror', 'knives', 'garcia', 'memory', 'amores', 'meiuva', 'forset', 'dragon', 'viesse', 'rambul', 'repsev', 'aivulp', 'matzah', 'family', 'doctor', 'morito', 'bubbles', 'clouds', 'scythe', 'pocket', 'kiseru', 'sewing', 'bottle', 'stella', 'palace', 'regalo', 'silver', 'clinic', 'prayer', 'swords', 'ballad', 'sanrio', 'collab', 'mithra', 'church', 'castle', 'bandit', 'arthur',
+  'pajama',
+  'cavern',
+  'master',
+  'winter',
 ]
